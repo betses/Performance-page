01be37b8b1cdfa5ac6c51af8c3721fc9cdc14cff
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-03-29 18:59:27 +0300

    Add circle features

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import emoji from "./assets/emoji _briefcase_.png";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="px-8">
+      <div className="">
+        <div className="h-[104px] w-[104px] rounded-full bg-[#FFF952] absolute" />
+        <div className="relative left-8 top-5 w-[488px]">
+          <h1 className="text-5xl font-bold">
+            Let’s see what the stats say about our performance
+          </h1>
+          <p className="pt-2 text-lg font-normal">
+            It’s always sunny with Fox Events!
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="grid grid-cols-12 gap-10">
+        <div className="w-[469px] h-[469px] bg-[#E5AF22] flex flex-col justify-center pl-[100px] rounded-full self-center col-span-5">
+          <h2 className="font-bold text-white text-9xl">281+</h2>
+          <p className="text-xl font-bold">Engaging Experiences </p>
+          <p className="text-base w-[280px]">
+            Vorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="w-[695px] h-[695px] bg-[#FFF952] flex flex-col pt-[180px] pl-[176px] rounded-full col-span-7">
+          <h2 className="font-bold text-white text-9xl">
+            49000 <br /> Hours
+          </h2>
+          <p className="text-xl font-bold">Experience Organization</p>
+          <p className="text-base w-[280px]">
+            Vorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <div className="relative bottom-[43%] right-[42%] flex rounded-full shadow-lg items-center justify-center w-24 h-24 bg-white">
+            <img className="" src={emoji} alt="breifcase" />
+          </div>
+          <div className="relative bottom-[30%] left-[84%] flex rounded-full shadow-lg items-center justify-center w-24 h-10 bg-white">
+            <p>Birthdays</p>
+          </div>
+          <div className="relative bottom-9 left-[52%] flex rounded-full shadow-lg items-center justify-center w-44 h-10 bg-white">
+            <p>Corporate Retreats</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
